4349da2d288d2b16158589423590e758b9543e9e
Rohit Nishad <153318014+RNrepostery@users.noreply.github.com>
2025-04-03 09:34:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const routes = require('./Router/rolute')
 const categoryroutes = require("./Router/CategoryRoute.js")
 const productroutes = require("./Router/productRoutes")
 const connectDB = require('./Config/db');
+const path = require('path');
 const port = 3001;
 app.use(express.json()); // Parse JSON bodies
 app.use(cors()); // Enable CORS (Notice the parentheses)
@@ -20,6 +21,13 @@ connectDB();
 app.use('/user',routes)
 app.use('/admin',categoryroutes)
 app.use('/product',productroutes)
+/ Serve frontend static files
+app.use(express.static(path.join(__dirname, './client/build')));
+
+// Catch-all route to serve index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+});
 
 app.get("/home", (req, res) => {
     res.send("hello world");
